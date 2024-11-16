82a0be2ed0fcbe7fcbef05506dd4799a3efbc866
jjordanoc <joaquin.jordan@utec.edu.pe>
2024-11-15 19:25:38 -0500

    ignore sift dataset, start pseudocode

--- GraphNode.h
+++ GraphNode.h
@@ -5,9 +5,23 @@
 #ifndef FRESHDISKANN_GRAPHNODE_H
 #define FRESHDISKANN_GRAPHNODE_H
 
+#include <vector>
 
 class GraphNode {
+    size_t id;
+    double distance;
+    bool expanded;
+public:
+    GraphNode(unsigned id, float distance) : id{id}, distance{distance}, expanded(false) {
+    }
 
+    inline bool operator<(const GraphNode &other) const {
+        return distance < other.distance || (distance == other.distance && id < other.id);
+    }
+
+    inline bool operator==(const GraphNode &other) const {
+        return (id == other.id);
+    }
 };
 
 
--- Index.cpp
+++ Index.cpp
@@ -3,8 +3,22 @@
 //
 
 #include "Index.h"
+#include <set>
 
 void Index::insert(const Vector &xq, const GraphNode &s, size_t searchListSize, double alpha, size_t outDegreeBound) {
 
 
+}
+
+std::pair<std::vector<GraphNode>, std::vector<GraphNode>>
+Index::greedySearch(const GraphNode &s, const Vector &xq, size_t k, size_t searchListSize) {
+    std::set<GraphNode> approxNearestNeighbors, visitedNodes, difference;
+    approxNearestNeighbors.insert(s);
+
+    do {
+        std::set_difference(approxNearestNeighbors.begin(), approxNearestNeighbors.end(), visitedNodes.begin(), visitedNodes.end(), difference.begin())
+    } while ()
+
+
+    return std::pair<std::vector<GraphNode>, std::vector<GraphNode>>();
 }
