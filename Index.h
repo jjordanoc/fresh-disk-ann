3624e4bbc3c3083b506a1c74dd93b4f628f5e491
Jerimy2021 <jerimy.sandoval@utec.edu.pe>
2024-11-18 22:59:24 -0500

    Metodo delete mejorado

--- Index.h
+++ Index.h
@@ -1,6 +1,7 @@
 #ifndef INDEX_H
 #define INDEX_H
 
+#include <cstddef>
 #pragma once
 #include "GraphNode.h"
 #include <vector>
@@ -13,8 +14,10 @@ public:
     std::pair<std::vector<GraphNode>, std::vector<GraphNode>> greedySearch(const GraphNode &s, const GraphNode &xq, size_t k, size_t searchListSize);
     double distance(const GraphNode &node, const GraphNode &xq);
     void robustPrune(const GraphNode &p, std::vector<GraphNode*> &v, double alpha, size_t outDegreeBound);
+    void deleteNodes(const std::unordered_set<int>& deleteList, double alpha, size_t outDegreeBound);
 private:
-
+    std::unordered_map<int, GraphNode*> graphNodes;
+    std::unordered_set<int> deleteList;
 };
 
 
